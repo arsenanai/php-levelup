63b919f4b189eab006472a358f54f2d70e4f0b45
Arsen Anay <arsenanai@gmail.com>
2023-01-14 19:55:07 +0600

    xdebug is installed, and tested

---
+++ xdebug/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+1) vizit and install xdebug from:
+https://xdebug.org/docs/install
+2) I've installed an extension on VS Code: PHP Debug
+3) Basic usage tutorial is here:
+https://laracasts.com/series/visual-studio-code-for-php-developers/episodes/13
+*/
+
+// set debugging points to test
+$a = 1;
+$b = $a + 2; // 3
+$c = $b * 3; // 9
+$d = $c ** 4; // 6561
+exit(); 
